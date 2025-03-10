646203a5bc6d91835b196579fdf1e64e3460b64a
ScreamOff <shadoow.clank@interia.pl>
2025-03-10 19:25:17 +0100

    dodanie loginu

--- src/main/java/com/example/demo/Server/Controllers/IndexController.java
+++ src/main/java/com/example/demo/Server/Controllers/IndexController.java
@@ -4,14 +4,12 @@ import com.example.demo.Server.Models.AppUsers;
 import com.example.demo.Server.Service.AppUserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
